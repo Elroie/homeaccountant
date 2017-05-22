ffcfc4976b492e3f1065f04b8973dca8900e1718
Guy <guyliberman@gmail.com>
2017-05-22 11:06:54 -0700

    for omri

--- web/app/home/controllers/about.controller.js
+++ web/app/home/controllers/about.controller.js
@@ -1,9 +1,16 @@
 'use strict';
 
-angular.module('app.home').controller('AboutController', ['$scope', '$timeout', 'Upload', function ($scope, $timeout, Upload) {
+angular.module('app.home').controller('AboutController', ['$scope', '$timeout', '$http', function ($scope, $timeout, $http) {
 
 
-    var ctrl = this;
-
+   var ctrl = this;
+   $http({
+        method : "GET",
+        url : "http://127.0.0.1:5000/api/allnotes"
+        }).then(function mySucces(response) {
+            ctrl.text = response.data[0];
+        }, function myError(response) {
+            $scope.error = response.statusText;
+        });
 
 }]);
