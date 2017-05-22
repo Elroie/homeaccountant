5104cc49e9b34b23d7e0f6c29558a4a820f50342
Omer Gez <omer.gez22@gmail.com>
2017-05-22 12:23:56 +0300

    dont use socket

--- homeaccountnant.sh
+++ homeaccountnant.sh
@@ -2,4 +2,4 @@
 
 cd /homeaccountant
 
-/usr/local/bin/gunicorn --workers 3 --bind unix:homeaccountant.sock -m 007 "create_app()"
+/usr/local/bin/gunicorn --workers 3 --bind 127.0.0.1:33014 "create_app()"
