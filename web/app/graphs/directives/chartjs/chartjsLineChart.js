df5daeed0296a740e8c7c02d68d816253f211312
Guy <guyliberman@gmail.com>
2017-06-02 00:03:08 -0700

    graphs

--- web/app/graphs/directives/chartjs/chartjsLineChart.js
+++ web/app/graphs/directives/chartjs/chartjsLineChart.js
@@ -3,6 +3,7 @@
 angular.module('app.graphs').directive('chartjsLineChart', ['$http', function ($http) {
 
     var graphdata;
+    var forecastdata;
 
     return {
         restrict: 'A',
@@ -12,7 +13,8 @@ angular.module('app.graphs').directive('chartjsLineChart', ['$http', function ($
             url : "/api/graph/getdata"
             }).then(function mySuccess(response) {
 
-                graphdata = response.data;
+                graphdata = response.data[0];
+                forecastdata = response.data[1];
 
                 // LINE CHART
                 // ref: http://www.chartjs.org/docs/#line-chart-introduction
@@ -78,6 +80,36 @@ angular.module('app.graphs').directive('chartjsLineChart', ['$http', function ($
                             pointHighlightFill: "#fff",
                             pointHighlightStroke: "rgba(169, 3, 41,1)",
                             data: [graphdata[0].other_price, graphdata[1].other_price, graphdata[2].other_price, graphdata[3].other_price, graphdata[4].other_price, graphdata[5].other_price, graphdata[6].other_price, graphdata[7].other_price,graphdata[8].other_price,graphdata[9].other_price,graphdata[10].other_price,graphdata[11].other_price]
+                        },
+                        {
+                            label: "Forecast-Electricity",
+                            fillColor: "rgba(255,255,102,0.2)",
+                            strokeColor: "rgba(255,255,102,0.5)",
+                            pointColor: "rgba(255,255,102,0.5)",
+                            pointStrokeColor: "#fff",
+                            pointHighlightFill: "#fff",
+                            pointHighlightStroke: "rgba(169, 3, 41,1)",
+                            data: [forecastdata[0].electricity_price, forecastdata[1].electricity_price, forecastdata[2].electricity_price, forecastdata[3].electricity_price, forecastdata[4].electricity_price, forecastdata[5].electricity_price, forecastdata[6].electricity_price,forecastdata[7].electricity_price,forecastdata[8].electricity_price,forecastdata[9].electricity_price,forecastdata[10].electricity_price,forecastdata[11].electricity_price]
+                        },
+                        {
+                            label: "Forecast-Water",
+                            fillColor: "rgba(204, 229, 255,0.2)",
+                            strokeColor: "rgba(204, 229, 255,0.5)",
+                            pointColor: "rgba(204, 229, 255,0.5)",
+                            pointStrokeColor: "#fff",
+                            pointHighlightFill: "#fff",
+                            pointHighlightStroke: "rgba(169, 3, 41,1)",
+                            data: [forecastdata[0].water_price, forecastdata[1].water_price, forecastdata[2].water_price, forecastdata[3].water_price, forecastdata[4].water_price, forecastdata[5].water_price, forecastdata[6].water_price,forecastdata[7].water_price,forecastdata[8].water_price,forecastdata[9].water_price,forecastdata[10].water_price,forecastdata[11].water_price]
+                        },
+                        {
+                            label: "Forecast-Other",
+                            fillColor: "rgba(255, 204, 204,0.2)",
+                            strokeColor: "rgba(255, 204, 204,0.5)",
+                            pointColor: "rgba(255, 204, 204,0.5)",
+                            pointStrokeColor: "#fff",
+                            pointHighlightFill: "#fff",
+                            pointHighlightStroke: "rgba(169, 3, 41,1)",
+                            data: [forecastdata[0].other_price, forecastdata[1].other_price, forecastdata[2].other_price, forecastdata[3].other_price, forecastdata[4].other_price, forecastdata[5].other_price, forecastdata[6].other_price,forecastdata[7].other_price,forecastdata[8].other_price,forecastdata[9].other_price,forecastdata[10].other_price,forecastdata[11].other_price]
                         }
                     ]
                 };
